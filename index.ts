3ad5094587bda2734fa5e227efaf65bb935a53cd
yamatoto <mftb35ymt@yahoo.co.jp>
2024-12-19 11:51:58 +0900

    comment

--- index.ts
+++ index.ts
@@ -7,8 +7,11 @@ const port = 3000;
 app.get('/', async (req, res) => {
     const ids = await getIdListFromS3();
     res.send(ids);
+    // aaaaaconflict!!!!
 });
-// ,,,
+
 app.listen(port, () => {
+    //aaa conflict!!!!
     console.log(`Example app listening on port ${port}`);
+    //aaaa conflict!!!!
 });
